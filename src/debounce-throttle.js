f9d8ea89b26edbef44974e1a999add9aca5e763f
Mr.huang <652463887@qq.com>
2021-05-31 22:28:57 +0800

    add throttle

---
+++ src/debounce-throttle.js
@@ -0,0 +1,36 @@
+/**
+ * 防抖 延迟指定时间time后执行函数fn，如果指定时间time内再次触发函数fn，则重新计算时间,即高频触发fn时只执行最后一次
+ * @param {Function} fn 要触发的函数
+ * @param {Number} time 指定时间
+ * @returns {Function}
+ */
+export const debounce = (fn, time = 300) => {
+    let timer = null
+    return function (...args) {
+        timer !== null && clearTimeout(timer)
+        timer = setTimeout(() => {
+            timer = null
+            fn.call(this, ...args)
+        }, time)
+    }
+}
+
+/**
+ * 节流 高频触发函数fn，在指定时间time内只执行一次
+ * @param {Function} fn 触发的函数
+ * @param {Number} time 指定时间
+ * @returns {Function}
+ */
+export const throttle = (fn, time = 300) => {
+    let isRuning = false
+    return function(...args){
+        if(isRuning){
+            return
+        }
+        isRuning = true
+        fn.call(this, ...args)
+        setTimeout(() => {
+            isRuning = false
+        }, time)
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
-import {sleep} from "./sleep.js"
-console.log("start");
-await sleep(2000)
-console.log("end");
+import { sleep } from "./sleep.js"
+// console.log("start");
+// await sleep(2000)
+// console.log("end");
+
+import { debounce, debouncefff } from './debounce-throttle.js'
+var obj = {
+  aaa: 33,
+  log: function(count){
+    console.log('log count', count);
+    return count
+  }
+}
+let log2 = debouncefff(obj.log, 300, true)
+
+for (let i = 0; i < 10; i++) {
+  await sleep(200)
+  // console.log('start', i);
+  let s = log2.call(obj, i)
+}
